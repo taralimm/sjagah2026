162dd871cafb5b5b6abce244b4771bcbcc9c4f94
taralimm <tara.marie.freelancer@gmail.com>
2026-05-19 00:53:47 -0700

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,3 @@
 import { app } from "../server";
 
-// This file is the entry point for Vercel serverless functions.
-// Any request to /api/* will be routed here via vercel.json rewrites.
-
 export default app;
